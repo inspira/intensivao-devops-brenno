2346acc3801c499f2eef6fda419ae0383a15b788
Brenno Andrade Novais <brenno.novais@inspira.com.br>
2024-03-01 12:19:51 -0300

    Adiciona tratamento de erros a script

--- server_on.sh
+++ server_on.sh
@@ -2,10 +2,19 @@
 
 ### TURN ON FRONTEND ###
 
-echo Turning Frontend On \n\n
+echo -e "echo Turning Frontend On\n\n"
 
 cd frontend || exit
 yarn start &
+
+if [ $? -eq 0 ]; then
+    echo "Frontend is on."
+else
+    echo "An error occurred while turning Frontend on."
+    exit 1
+fi
+
+echo -e "-------------------------------------------------------------------\n\n"
 
 ### TURN ON BACKEND ###
 
@@ -14,6 +23,14 @@ echo Turning Backend On \n\n
 
 cd ../backend || exit
 pipenv shell
+pipenv install
 uvicorn main:app --reload &
+
+if [ $? -eq 0 ]; then
+    echo "Backend is on."
+else
+    echo "An error occurred while turning Backend on."
+    exit 1
+fi
 
 cd ..
