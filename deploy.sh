1f2ecb4cca9c03c3d56a329efbc425a148a958f1
Brenno Andrade Novais <brenno.novais@inspira.com.br>
2024-03-01 15:08:46 -0300

    Habilita NGINX

--- deploy.sh
+++ deploy.sh
@@ -15,36 +15,63 @@ export NVM_DIR="$HOME/.nvm"
 
 nvm install
 
-echo -e "\n\nInstalling YARN \n\n"
+echo -e "==============================================================="
+echo -e "Installing YARN"
+echo -e "===============================================================\n"
 
 npm install --global yarn
 
 echo -e "==============================================================="
-echo -e "\n\nInstalling Frontend dependencies"
+echo -e "Installing Frontend dependencies"
 echo -e "===============================================================\n"
 
 yarn install
+
+
+echo -e "==============================================================="
+echo -e "Building frontend"
+echo -e "===============================================================\n"
+
+yarn build:deploy
 
 ### BACKEND SETUP ###
 
 cd ../backend || exit
 
 echo -e "==============================================================="
-echo -e "\n\nInstalling Pipenv"
-echo -e "===============================================================\n"
-
-python3 -m venv sum-numbers
-
-echo -e "==============================================================="
-echo -e "Activating Virtual Env"
-echo -e "===============================================================\n"
-
-source sum-numbers/bin/activate
-
-echo -e "==============================================================="
-echo -e "\n\nInstalling Backend Dependencies"
+echo -e "Installing Backend Dependencies"
 echo -e "===============================================================\n"
 
 pip install -r requirements.txt
 
 cd ..
+
+### NGINX SETUP ###
+
+echo -e "==============================================================="
+echo -e "Installing NGINX"
+echo -e "===============================================================\n"
+
+sudo apt update
+sudo apt install nginx
+
+echo -e "==============================================================="
+echo -e "Configuring Server"
+echo -e "===============================================================\n"
+
+cp -f nginx_config /etc/nginx/sites-available/sum-numbers
+sudo ln -s /etc/nginx/sites-available/sum-numbers /etc/nginx/sites-enabled/
+
+echo -e "==============================================================="
+echo -e "Starting nginx"
+echo -e "===============================================================\n"
+
+sudo nginx -t
+
+if [ $? -eq 0 ]; then
+    sudo systemctl reload nginx
+    echo "Server is on."
+else
+    echo "An error occurred while turning the server on."
+    exit 1
+fi
