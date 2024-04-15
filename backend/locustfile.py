351fd97f1b011d85053f414b85519eea4d7aa8d7
Brenno Andrade Novais <brenno.novais@inspira.com.br>
2024-04-15 01:47:34 -0300

    Add stress test pipeline

--- backend/locustfile.py
+++ backend/locustfile.py
@@ -1,10 +1,17 @@
-import time
+import os
 
+from dotenv import load_dotenv
 from locust import HttpUser, between, task
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(BASEDIR, '.env'))
+
+base_url = os.environ.get("BASE_URL")
 
 
 class QuickstartUser(HttpUser):
     wait_time = between(1, 5)
+    host = base_url
 
     @task
     def health(self):
