d9bc27ab11a9e7d48b91c31d6de485fb654f7372
Brenno Andrade Novais <brenno.novais@inspira.com.br>
2024-04-15 13:18:25 -0300

    Fix locust test

--- backend/locustfile.py
+++ backend/locustfile.py
@@ -1,25 +1,31 @@
-import os
-
-from dotenv import load_dotenv
 from locust import HttpUser, between, task
-
-BASEDIR = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(BASEDIR, '.env'))
-
-base_url = os.environ.get("BASE_URL")
 
 
 class QuickstartUser(HttpUser):
     wait_time = between(1, 5)
+    health_success_count = 0
+    health_total_requests = 0
+    sum_success_count = 0
+    sum_total_requests = 0
 
     @task
     def health(self):
-        self.client.get("/api/health")
+        response = self.client.get("/api/health")
+        self.health_total_requests += 1
+
+        if response.status_code == 200:
+            self.health_success_count += 1
 
     @task(3)
     def sum(self):
-        self.client.post("/api/sum", json={"num1": 10, "num2": 50})
+        response = self.client.post("/api/sum", json={"num1": 10, "num2": 50})
+        self.sum_total_requests += 1
 
-    @property
-    def host(self):
-        return os.environ.get("BASE_URL", "http://127.0.0.1:8000")
+        if response.status_code == 200:
+            self.sum_success_count += 1
+
+    def on_stop(self):
+        health_success_rate = self.health_success_count / self.health_total_requests
+        sum_success_rate = self.sum_success_count / self.sum_total_requests
+        assert health_success_rate > 0.95, f"Health success rate: {health_success_rate}"
+        assert sum_success_rate > 0.95, f"Sum success rate: {sum_success_rate}"
